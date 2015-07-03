f46dedb9112b9c59d75d71db5e23de68c2bc2ecd
Yuhuang Hu <duguyue100@gmail.com>
2015-07-03 11:47:25 +0800

    update description

--- neuralfeature.lua
+++ neuralfeature.lua
@@ -9,7 +9,26 @@ local image=require 'image';
 
 neuralfeature={};
 
+--[[The function extracts features from choosen layer
+
+Parameters
+----------
+net_model : object
+	a neural network model
+image_adds : table
+	a list of path of images
+layer_id : integer
+	chosen layer of the network 
+	
+Returns
+-------
+out_feature : table
+	list of extracted features
+out_labels : table
+	list of predicted labels by pre-trained model
+]]
 function neuralfeature.extract(net_model, image_adds, layer_id)
+
 	-- default working with last fc layer
 	local layer_id = layer_id or #net.modules-1;
 	
