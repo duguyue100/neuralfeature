e13949728a7c54bc31051e1b41e7e5fee4e09696
Yuhuang Hu <duguyue100@gmail.com>
2015-07-06 00:35:20 +0800

    update for ffmpeg test

--- scripts/ffmpeg_test.lua
+++ scripts/ffmpeg_test.lua
@@ -1,8 +1,24 @@
 -- FFMPEG test
 
 require 'ffmpeg'
+require 'image'
 
 vid=ffmpeg.Video('../data/000046280.avi');
 
+-- print number of frames
+
+print(vid.nframes);
+
+-- dumpy all as tensor
+
+content=vid:totensor{}
+
+-- display tensor
+image.display(content[10])
+
+-- play the video
+
 vid:play{}
 
+-- for more information, please read code of:
+-- https://github.com/clementfarabet/lua---ffmpeg/blob/master/init.lua
